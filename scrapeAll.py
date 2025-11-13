f9c731694f145d21732f81950d588cd2d783f2b8
aph <79864599+aphcore@users.noreply.github.com>
2025-11-12 23:30:05 -0800

    cache !
    cache in json + memory

--- scrapeAll.py
+++ scrapeAll.py
@@ -1,11 +1,34 @@
 import requests
 import pandas
 import time
+import json
+import os
 from requests.exceptions import RequestException, ConnectionError
 
-# YYYYYYYY format season ID; 20252026, 20242025, ect. 
+# YYYYYYYY format season ID; 20252026, 20242025, ect.
 # previous seasons with more games will take more time :(
 SEASON = "20252026"
+
+# cache file path
+CACHE_FILE = f"nhl_cache_{SEASON}.json"
+
+
+# load cache from file
+def load_cache():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+
+# save cache to file
+def save_cache():
+    with open(CACHE_FILE, 'w') as f:
+        json.dump(cache, f)
+
+
+# simple in-memory cache - loads from file when module is imported
+cache = load_cache()
 
 # team configs from NHL API
 teams = [
@@ -43,6 +66,7 @@ teams = [
     {"name": "Utah Mammoth", "abbr": "UTA", "id": 68},
 ]
 
+
 # this section of code is to scrape the data from NHL API
 
 # for use instead of requests.get(), adds retry and backoff to amend server disconnection issues
@@ -58,16 +82,31 @@ def safe_get(url, max_retries=5, backoff=2):
             time.sleep(wait)
     raise RuntimeError(f"Failed to fetch {url} after {max_retries} retries")
 
+
 # get players
 def get_team_roster(team_abbr, team_name):
     url = f"https://api-web.nhle.com/v1/roster/{team_abbr}/{SEASON}"
-    resp = safe_get(url)
-    # resp contains status code, check status code not an error; aka HTTP successful
-    resp.raise_for_status()
-    data = resp.json()
+
+    # check cache
+    if url in cache:
+        resp = safe_get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        if cache[url] == data:
+            print(f"Roster unchanged for {team_name}, skipping")
+            return None
+        cache[url] = data
+        save_cache()
+    else:
+        resp = safe_get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        cache[url] = data
+        save_cache()
+
     players = data.get("forwards", []) + data.get("defensemen", []) + data.get("goalies", [])
     df = pandas.DataFrame(players)
-    
+
     # parses player name from dict/string -> string
     def get_name(field):
         if isinstance(field, dict):
@@ -82,32 +121,68 @@ def get_team_roster(team_abbr, team_name):
     print(f"Loaded {len(id_to_name)} {team_name} players")
     return id_to_name
 
+
 # get schedule for given team
 def get_team_schedule(team_abbr, team_name):
     url = f"https://api-web.nhle.com/v1/club-schedule-season/{team_abbr}/{SEASON}"
-    resp = safe_get(url)
-    # make sure the http request was successful
-    resp.raise_for_status()
+
+    # check cache
+    if url in cache:
+        resp = safe_get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        if cache[url] == data:
+            print(f"Schedule unchanged for {team_name}, skipping")
+            return None
+        cache[url] = data
+        save_cache()
+    else:
+        resp = safe_get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        cache[url] = data
+        save_cache()
+
     # flatten JSON data into df
-    schedule_data = pandas.json_normalize(resp.json(), "games")
+    schedule_data = pandas.json_normalize(data, "games")
     # only use completed games
     played_games = schedule_data[schedule_data["gameState"].str.startswith("OFF")]
     print(f"Found {len(played_games)} completed games for {team_name}")
     return played_games
 
+
 # get goal info for given team (sorting out goals by the other team)
 def get_team_goals(team_id, played_games):
     goal_data = []
-    # for each game 
+    # for each game
     for _, game in played_games.iterrows():
         game_id = game["id"]
         date = game["gameDate"]
         url = f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play"
-        resp = safe_get(url)
-        if resp.status_code != 200:
-            continue
+
+        # check cache
+        if url in cache:
+            resp = safe_get(url)
+            if resp.status_code != 200:
+                continue
+            data = resp.json()
+            if cache[url] == data:
+                # game unchanged, use cached result
+                plays = data.get("plays", [])
+            else:
+                cache[url] = data
+                save_cache()
+                plays = data.get("plays", [])
+        else:
+            resp = safe_get(url)
+            if resp.status_code != 200:
+                continue
+            data = resp.json()
+            cache[url] = data
+            save_cache()
+            plays = data.get("plays", [])
+
         # only keep scoring info
-        plays = resp.json().get("plays", [])
         for p in plays:
             if p.get("typeDescKey") == "goal":
                 details = p.get("details", {})
@@ -123,71 +198,76 @@ def get_team_goals(team_id, played_games):
     df = pandas.DataFrame(goal_data)
     return df
 
+
 # the below code is to get correlation data
 
 # loop through teams
-for team in teams:
-    name = team["name"]
-    abbr = team["abbr"]
-    tid = team["id"]
+def scrape():
+    for team in teams:
+        name = team["name"]
+        abbr = team["abbr"]
+        tid = team["id"]
 
-    print(f"\nProcessing {name} ({abbr})")
+        print(f"\nProcessing {name} ({abbr})")
 
-    # roster and mapping
-    id_to_name = get_team_roster(abbr, name)
-
-    # schedule and completed games
-    played_games = get_team_schedule(abbr, name)
-    if played_games.empty:
-        print(f"No completed games for {name}")
-        continue
-
-    # goal data
-    goals_df = get_team_goals(tid, played_games)
-    if goals_df.empty:
-        print(f"No {name} goal data")
-        continue
-
-    # map IDs -> names
-    for col in ["scorer", "assist1", "assist2"]:
-        goals_df[col] = goals_df[col].map(id_to_name).fillna(goals_df[col])
-
-    # compute assist -> any point on same goal 
-    # dropna gets rid of blank assists
-    # unique gets rid of duplicate names; ie one name per player who's ever gotten an assist/point
-    assisters = pandas.Series(pandas.concat([goals_df['assist1'], goals_df['assist2']])).dropna().unique()
-    players_all = pandas.Series(pandas.concat([goals_df['scorer'], goals_df['assist1'], goals_df['assist2']])).dropna().unique()
-    # create a data frame with a row per assist-getter, and a column for anyone who's gotten a point this season
-    co_assist_point = pandas.DataFrame(index=assisters, columns=players_all, dtype=float)
-
-    for assister in assisters:
-        # get all goals where this player registers an assist
-        goals_with_assist = goals_df[
-            (goals_df['assist1'] == assister) | (goals_df['assist2'] == assister)
-        ]
-        # get total assist count
-        n_assists = len(goals_with_assist)
-        # incase we somehow got someone without an assist
-        if n_assists == 0:
-            co_assist_point.loc[assister] = 0
+        # roster and mapping
+        id_to_name = get_team_roster(abbr, name)
+        if id_to_name is None:
             continue
-        # for each other player who's recorded a point this season, 
-        # count the number of points recorded on the same goal as our assister
-        for player in players_all:
-            count = (
-                (goals_with_assist['scorer'] == player) |
-                (goals_with_assist['assist1'] == player) |
-                (goals_with_assist['assist2'] == player)
-            ).sum()
-            # get probability
-            co_assist_point.loc[assister, player] = count / n_assists
 
-    # set diagonals to 0; we don't want to suggest betting on the same player twice
-    for assister in assisters:
-        if assister in co_assist_point.columns:
-            co_assist_point.loc[assister, assister] = 0.0
+        # schedule and completed games
+        played_games = get_team_schedule(abbr, name)
+        if played_games is None or played_games.empty:
+            print(f"No new completed games for {name}")
+            continue
 
-    # save to csv for suggestion program to use
-    out_path = f"{abbr}_assist_point_same_goal_{SEASON}.csv"
-    co_assist_point.to_csv(out_path)
-    print(f"saved as {out_path}")
+        # goal data
+        goals_df = get_team_goals(tid, played_games)
+        if goals_df.empty:
+            print(f"No {name} goal data")
+            continue
+
+        # map IDs -> names
+        for col in ["scorer", "assist1", "assist2"]:
+            goals_df[col] = goals_df[col].map(id_to_name).fillna(goals_df[col])
+
+        # compute assist -> any point on same goal
+        # dropna gets rid of blank assists
+        # unique gets rid of duplicate names; ie one name per player who's ever gotten an assist/point
+        assisters = pandas.Series(pandas.concat([goals_df['assist1'], goals_df['assist2']])).dropna().unique()
+        players_all = pandas.Series(
+            pandas.concat([goals_df['scorer'], goals_df['assist1'], goals_df['assist2']])).dropna().unique()
+        # create a data frame with a row per assist-getter, and a column for anyone who's gotten a point this season
+        co_assist_point = pandas.DataFrame(index=assisters, columns=players_all, dtype=float)
+
+        for assister in assisters:
+            # get all goals where this player registers an assist
+            goals_with_assist = goals_df[
+                (goals_df['assist1'] == assister) | (goals_df['assist2'] == assister)
+                ]
+            # get total assist count
+            n_assists = len(goals_with_assist)
+            # incase we somehow got someone without an assist
+            if n_assists == 0:
+                co_assist_point.loc[assister] = 0
+                continue
+            # for each other player who's recorded a point this season,
+            # count the number of points recorded on the same goal as our assister
+            for player in players_all:
+                count = (
+                        (goals_with_assist['scorer'] == player) |
+                        (goals_with_assist['assist1'] == player) |
+                        (goals_with_assist['assist2'] == player)
+                ).sum()
+                # get probability
+                co_assist_point.loc[assister, player] = count / n_assists
+
+        # set diagonals to 0; we don't want to suggest betting on the same player twice
+        for assister in assisters:
+            if assister in co_assist_point.columns:
+                co_assist_point.loc[assister, assister] = 0.0
+
+        # save to csv for suggestion program to use
+        out_path = f"{abbr}_assist_point_same_goal_{SEASON}.csv"
+        co_assist_point.to_csv(out_path)
+        print(f"saved as {out_path}")
